b5d5bc07a09ada325d02618d77b92a3479fdb13a
Karol Kochaski <karolkochanski@gmail.com>
2020-12-22 13:54:43 +0100

    setup env for android and ios Firebase

---
+++ components/WaterFill.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+ 
+export default function WatterFill() {
+  const [watter, setWatter] = useState("");
+ 
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.addWater}>
+        <Text style={styles.waterText}>Add water</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+ 
+  addWater: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FF1493",
+  },
+});
--- ios/waterFiller/AppDelegate.m
+++ ios/waterFiller/AppDelegate.m
@@ -30,12 +30,13 @@ static void InitializeFlipper(UIApplication *application) {
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
-
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"waterFiller"
                                             initialProperties:nil];
 
+  [FIRApp configure];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
